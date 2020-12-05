ef7dfe7992f9d7fb5a837c5b1a4c9fd828114ed8
PeterJacobsenDenmark <peter@peteroglone.dk>
2020-12-05 15:47:47 +0100

    update

--- main.py
+++ main.py
@@ -3,36 +3,34 @@
 #  
 #  
 
+import dropbox
+from dropbox.files import WriteMode
 
-import threading
-import time
-import sys
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
 
-import RPi.GPIO as GPIO
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
 
-from multiprocessing import Queue, Value #Needed for cross thread value communnication
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
 
-from UploadThread import startUploadThread
+def upload():
+    access_token = 'c4g63lFy7KoAAAAAAAAAAUeWmLmnXGMtk5N4nskzPXdNVRZCdsnKIcjh3vNFh4L3'
+    transferData = TransferData(access_token)
 
+    file_from = '/home/pi/WeatherPi/log.txt' # This is name of the file to be uploaded
+    file_to = '/log.txt'  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
 
-
+    # API v2
+    transferData.upload_file(file_from, file_to)
 
 
 def main():
-    try:       
-        print( "READ OUT")
-
-        uploadThread = threading.Thread( target=startUploadThread, args=( ))
-        uploadThread.start()
-
-
-    except (KeyboardInterrupt, SystemExit):
-        print( "MAIN - Keyboard interrup " )
-    except Exception as e:
-        error_log(e, "Unhandled Exception in Main")
-        if not debug:
-            time.sleep(60)
-            reboot()
+   
+  while True:
+      upload()
 
 
 
